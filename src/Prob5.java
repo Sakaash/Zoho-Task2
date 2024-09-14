99c39b13951a5327f9606a6ce46b647992f093ac
Sakaash <ssakaash03@gmail.com>
2024-09-14 21:10:13 +0530

    Updated

--- src/Prob5.java
+++ src/Prob5.java
@@ -1,9 +1,16 @@
 public class Prob5 {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 2;
-        int c = 3;
-        int discriminant = (int)Math.sqrt(Math.pow(b,2)-(4*a*c));
-        System.out.println("Positive root : " + ((-b+discriminant)/(2*a)));
+        int[] array = {2,5,7};
+        int b2 = (int)Math.pow(array[1], 2);
+        int ac = array[0]*array[2];
+        int a2 = 2*array[0];
+        int posNumerator = -array[1] + (int)Math.sqrt(b2-(4*ac));
+        int negNumerator = -array[1] - (int)Math.sqrt(b2-(4*ac));
+        int denominator = a2;
+
+        int answer =  posNumerator / denominator;
+        int answer2 = negNumerator / denominator;
+        System.out.println(answer);
+        System.out.println(answer2);
     }    
 }
